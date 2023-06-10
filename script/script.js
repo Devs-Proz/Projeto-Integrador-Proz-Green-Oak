68bd6765d1a7b67587bb159153138e3e9e258bb4
Laise France <laise94@hotmail.com>
2023-06-10 13:34:27 -0300

    add paginas com redirecionamento

---
+++ script/script.js
@@ -0,0 +1,15 @@
+ // Função para carregar o navbar e o footer em todas as páginas
+ function loadCommonElements() {
+    fetch('./navbar.html')
+    
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById('navbar-container').innerHTML = html;
+      });
+
+    fetch('./footer.html')
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById('footer-container').innerHTML = html;
+      });
+  }
